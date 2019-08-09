28b79405d11f710e2fadd0f8308070f9ad9c756b
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-09 16:58:23 +0200

    add git pull

--- deploypanosc.sh
+++ deploypanosc.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+git pull
 helm del --purge lbpanosc
 helm install lb-panosc --name lbpanosc --namespace dev 
 # --set image.tag=latest --set image.repository garethcmurphy/lb-panosc
