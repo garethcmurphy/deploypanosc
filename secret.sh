18fc43d8efb856d78cf98b73be7df8d47eba7921
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-09 16:55:29 +0200

    edit s3ret

--- secret.sh
+++ secret.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout lb-panosc.key -out lb-panosc.crt -subj "/CN=scicat01.esss.lu.se" -days 3650
+	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout lbpanosc.key -out lb-panosc.crt -subj "/CN=scicat01.esss.lu.se" -days 3650
 
 
-	kubectl create secret -ndev tls lb-panoscservice --key lb-panosc.key --cert lb-panosc.crt
+	kubectl create secret -ndev tls lbpanoscservice --key lb-panosc.key --cert lb-panosc.crt
