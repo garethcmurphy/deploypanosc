9346371ec0495c6dc78ad2c243c64eeb801a18db
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-09 15:32:26 +0200

    rename

--- deploypanosc.sh
+++ deploypanosc.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-helm install lb-panosc --name lb-panosc --namespace dev --set image.tag=latest --set image.repository garethcmurphy/lb-panosc
+helm install lb-panosc --name lbpanosc --namespace dev --set image.tag=latest --set image.repository garethcmurphy/lb-panosc
